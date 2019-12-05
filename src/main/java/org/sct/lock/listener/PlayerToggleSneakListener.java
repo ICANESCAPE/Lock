33649f3ce962a1b2cbba868388717d57d45a20e2
LovesAsuna <625924077@qq.com>
2019-12-05 22:43:34 +0800

    [+] 添加玩家潜行监听器

---
+++ src/main/java/org/sct/lock/listener/PlayerToggleSneakListener.java
@@ -0,0 +1,18 @@
+package org.sct.lock.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.sct.lock.cache.Cache;
+
+public class PlayerToggleSneakListener implements Listener {
+    @EventHandler
+    public void onPlayerToggleSneak(PlayerToggleSneakEvent e) {
+        //如果玩家正在前行
+        if (e.isSneaking()) {
+            Cache.getPlayerisSneak().put(e.getPlayer(),true);
+        } else {
+            Cache.getPlayerisSneak().remove(e.getPlayer());
+        }
+    }
+}

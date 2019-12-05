47424b0476b022c7f47f0b74262aca66638e8d0e
LovesAsuna <625924077@qq.com>
2019-12-06 00:05:45 +0800

    [+] 监听玩家右键门的时间

--- src/main/java/org/sct/lock/listener/PlayerInteractListener.java
+++ src/main/java/org/sct/lock/listener/PlayerInteractListener.java
@@ -8,10 +8,12 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.sct.lock.cache.Cache;
 import org.sct.lock.enumeration.ConfigType;
 import org.sct.lock.file.Config;
+import org.sct.lock.util.CheckUtil;
 import org.sct.lock.util.LockUtil;
 
 import java.util.HashMap;
@@ -26,23 +28,42 @@ import static org.sct.lock.Lock.*;
  */
 public class PlayerInteractListener implements Listener {
 
-    private Map<Location, Player> lt_py = Cache.getLt_py();
-    private Map<Player,Location> py_lt = Cache.getPy_lt();
-
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e) {
-        List<String> doorList = Config.getStringList(ConfigType.SETTING_SIGNTYPE);
-        if (e.hasItem()) {//如果玩家手持物品
-            for (String materialString : doorList) {
-                Material material = Material.getMaterial(materialString);
-                if (e.getItem().getType() == material) {
-                    //System.out.println("牌子类型: " + material);//输出符合的牌子类型
-                    Bukkit.getScheduler().runTaskLater(getInstance(),()->{
-                        LockUtil.getLocation(e);
-                    },1L);
+        List<String> signList = Config.getStringList(ConfigType.SETTING_SIGNTYPE);
+        List<String> doorList = Config.getStringList(ConfigType.SETTING_DOORTYPE);
+        //如果玩家右键方块
+        if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+            //如果玩家手持物品
+            if (e.hasItem()) {
+                for (String sign : signList) {
+                    Material material = Material.getMaterial(sign);
+                    if (e.getItem().getType() == material) {
+                        //System.out.println("牌子类型: " + material);//输出符合的牌子类型
+                        Bukkit.getScheduler().runTaskLater(getInstance(),()->{
+                            LockUtil.getLocation(e);
+                        },1L);
+                    }
                 }
             }
+
+            for (String door : doorList) {
+                //如果右键的门符合类型
+                if (Cache.getPlayerisSneak().get(e.getPlayer()) == null || !Cache.getPlayerisSneak().get(e.getPlayer())) {
+                    return;
+                }
+                if (e.getClickedBlock().getType() == Material.getMaterial(door)) {
+                    Cache.getDoorLocation().put(e.getPlayer(),e.getClickedBlock().getLocation());
+                    CheckUtil.CheckSign(e.getPlayer(),e.getClickedBlock());
+                }
+            }
+
+
+
         }
+
+
+
     }
 
 

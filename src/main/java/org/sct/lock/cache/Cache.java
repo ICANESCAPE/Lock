37a74f15705bc1be062e57fc6749e7d634abe293
LovesAsuna <625924077@qq.com>
2019-12-06 12:55:18 +0800

    [+] 添加新的HashMap

--- src/main/java/org/sct/lock/cache/Cache.java
+++ src/main/java/org/sct/lock/cache/Cache.java
@@ -2,6 +2,7 @@ package org.sct.lock.cache;
 
 import lombok.Getter;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import java.util.HashMap;
 import java.util.Map;
@@ -13,14 +14,18 @@ public class Cache {
 
     @Getter private static Map<Location, Player> LocationPlayer;
     @Getter private static Map<Player, Location> PlayerLocation;
-    @Getter private static Map<Player, Location> DoorLocation;
+    //玩家潜行的状态
     @Getter private static Map<Player, Boolean> PlayerisSneak;
-
+    //玩家交互的门上方的牌子
+    @Getter private static Map<Player, Block> PlayerSign;
+    //玩家交互的门上方的方块(忽视高度)
+    @Getter private static Map<Player, Block> PlayerBlock;
     static {
         LocationPlayer = new HashMap<>();
         PlayerLocation = new HashMap<>();
-        DoorLocation = new HashMap<>();
         PlayerisSneak = new HashMap<>();
+        PlayerSign = new HashMap<>();
+        PlayerBlock = new HashMap<>();
     }
 
 

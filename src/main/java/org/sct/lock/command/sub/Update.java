c0c4097f5283d23a1f55044c186790de35d648df
LovesAsuna <625924077@qq.com>
2019-12-09 23:31:50 +0800

    [+] 添加获取更新信息功能

--- src/main/java/org/sct/lock/command/sub/Update.java
+++ src/main/java/org/sct/lock/command/sub/Update.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.sct.lock.Lock;
 import org.sct.lock.util.DownloadUtil;
+import org.sct.lock.util.GetUrlMessage;
 import org.sct.lock.util.SubCommand;
 
 import java.io.IOException;
@@ -12,17 +13,32 @@ public class Update implements SubCommand {
 
     @Override
     public boolean execute(CommandSender sender, String[] args) {
-        Runnable runnable = () ->{
-            try {
-                DownloadUtil.Download("https://github.com/ICANESCAPE/Lock/releases/download/" + Lock.getInstance().getDescription().getVersion() + "/Lock.jar","Lock.jar",Lock.getInstance().getDataFolder().getPath() + "\\update");
-            } catch (IOException e) {
-                e.printStackTrace();
-                sender.sendMessage("§7[§eLock§7]§c下载更新时出错");
+        if (args.length == 2) {
+            if (args.length == 2 && args[1].equalsIgnoreCase("download")) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            DownloadUtil.Download("https://github.com/ICANESCAPE/Lock/releases/download/" + Lock.getInstance().getDescription().getVersion() + "/Lock.jar","Lock.jar",Lock.getInstance().getDataFolder().getPath() + "\\update");
+                            sender.sendMessage("§7[§eLock§7]§2下载成功");
+                        } catch (IOException e) {
+                            sender.sendMessage("§7[§eLock§7]§c下载更新时出错");
+                        }
+                    }
+                }).start();
+            } else if (args.length == 2 && args[1].equalsIgnoreCase("version")) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            GetUrlMessage.get(sender);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
             }
-        };
-        new Thread(runnable).run();
-        sender.sendMessage("§7[§eLock§7]§2下载成功");
-
+        }
         return true;
     }
 }

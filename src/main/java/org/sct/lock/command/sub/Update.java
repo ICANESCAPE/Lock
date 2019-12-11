3d3a814d53f55e0086fc0791436d22c1314ebec0
LovesAsuna <625924077@qq.com>
2019-12-11 19:34:15 +0800

    [+] 修改多线程使用方式为线程池

--- src/main/java/org/sct/lock/command/sub/Update.java
+++ src/main/java/org/sct/lock/command/sub/Update.java
@@ -3,6 +3,7 @@ package org.sct.lock.command.sub;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.sct.lock.Lock;
+import org.sct.lock.data.LockData;
 import org.sct.lock.util.DownloadUtil;
 import org.sct.lock.util.GetUrlMessage;
 import org.sct.lock.util.SubCommand;
@@ -15,24 +16,25 @@ public class Update implements SubCommand {
     public boolean execute(CommandSender sender, String[] args) {
         if (args.length == 2) {
             if (args.length == 2 && args[1].equalsIgnoreCase("download")) {
-                new Thread( () -> {
+                LockData.getPool().submit(() -> {
                     try {
                         DownloadUtil.download("https://github.com/ICANESCAPE/Lock/releases/download/" + Lock.getInstance().getDescription().getVersion() + "/Lock.jar","Lock.jar",Lock.getInstance().getDataFolder().getPath() + "\\update");
                         sender.sendMessage("§7[§eLock§7]§2下载成功");
                     } catch (IOException e) {
                         sender.sendMessage("§7[§eLock§7]§c下载更新时出错");
                     }
-                }).start();
+                });
 
             } else if (args.length == 2 && args[1].equalsIgnoreCase("version")) {
-                new Thread( () -> {
+                LockData.getPool().submit(() -> {
                     try {
                         GetUrlMessage.get(sender);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-                }).start();
+                });
             }
+
         }
         return true;
     }

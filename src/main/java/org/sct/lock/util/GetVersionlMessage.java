caf2ab06df93ff28d9fda9674e90b47187e4efda
LovesAsuna <625924077@qq.com>
2019-12-12 15:30:20 +0800

    [*] 采用流的方式处理结果 [*] 更改类文件名

--- src/main/java/org/sct/lock/util/GetVersionlMessage.java
+++ src/main/java/org/sct/lock/util/GetVersionlMessage.java
@@ -11,25 +11,19 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class GetUrlMessage {
+public class GetVersionlMessage {
     public static void get(CommandSender sender) throws IOException {
         URL url = new URL("https://lovesasuna.github.io/update/Lock");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setConnectTimeout(3 * 1000);
         conn.connect();
         BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String read;
-        int line = 0;
-        List<String> version = new ArrayList<>();
-        while ((read = reader.readLine()) != null) {
-            line++;
-            if (line == 9) {
-                version = getMsg(read);
-            }
-        }
+        String version = reader.lines().parallel().skip(8).limit(1).collect(Collectors.toList()).get(0);
+        List<String> versionList = getMsg(version);
         sender.sendMessage("§e===============================================");
-        for (String versionMsg : version) {
+        for (String versionMsg : versionList) {
             sender.sendMessage(versionMsg);
         }
         sender.sendMessage("§e===============================================");

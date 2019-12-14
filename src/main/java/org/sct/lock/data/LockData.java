4b0c9849d854e93e40803ed68af9a514c3be60d0
LovesAsuna <625924077@qq.com>
2019-12-14 16:55:29 +0800

    [+] 添加计划线程池

--- src/main/java/org/sct/lock/data/LockData.java
+++ src/main/java/org/sct/lock/data/LockData.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * @author icestar
@@ -20,7 +21,9 @@ public class LockData {
         PlayerisSneak = new HashMap<>();
         PlayerSign = new HashMap<>();
         PlayerBlock = new HashMap<>();
+        inhibition = new HashMap<>();
         pool = Executors.newFixedThreadPool(5);
+        scheduledpool = Executors.newScheduledThreadPool(1);
     }
 
     @Getter private static Map<Location, Player> LocationPlayer;
@@ -39,5 +42,9 @@ public class LockData {
     /*插件专用线程池*/
     @Getter private static ExecutorService pool;
 
+    /*交互事件抑制器*/
+    @Getter private static Map<Player, Boolean> inhibition;
 
+    /*插件专用计划线程池*/
+    @Getter private static ScheduledExecutorService scheduledpool;
 }

12c2822abd5d7b2c4e5a57d1aba02d182e17e7cd
LovesAsuna <625924077@qq.com>
2020-01-29 19:17:33 +0800

    [+] 修复不支持linux的问题

--- src/main/java/org/sct/lock/file/Lang.java
+++ src/main/java/org/sct/lock/file/Lang.java
@@ -13,7 +13,7 @@ public class Lang {
     private static YamlConfiguration config;
 
     public static void loadLang() {
-        file = new File(Lock.getInstance().getDataFolder() + "\\lang.yml");
+        file = new File(Lock.getInstance().getDataFolder() + File.separator + "lang.yml");
         if (!file.exists()) { Lock.getInstance().saveResource("lang.yml",false); }
         config = YamlConfiguration.loadConfiguration(file);
     }

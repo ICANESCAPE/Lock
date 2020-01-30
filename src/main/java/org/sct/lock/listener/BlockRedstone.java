c7984be1fb031821b0ee68d80e722ed15ea180cf
LovesAsuna <625924077@qq.com>
2020-01-30 19:55:30 +0800

    [+] 修复不检查红石有关配置的问题

--- src/main/java/org/sct/lock/listener/BlockRedstone.java
+++ src/main/java/org/sct/lock/listener/BlockRedstone.java
@@ -3,11 +3,17 @@ package org.sct.lock.listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockRedstoneEvent;
+import org.sct.lock.enumeration.ConfigType;
+import org.sct.lock.file.Config;
 import org.sct.lock.util.CheckUtil;
 
 public class BlockRedstone implements Listener {
     @EventHandler
     public void onBlockRedstone(BlockRedstoneEvent e) {
+
+        if (!Config.getBoolean(ConfigType.SETTING_BANREDSTONEACTIVE)) {
+            return;
+        }
 
         if (CheckUtil.CheckSign(e.getBlock().getWorld(), null, e.getBlock())) {
             e.setNewCurrent(0);

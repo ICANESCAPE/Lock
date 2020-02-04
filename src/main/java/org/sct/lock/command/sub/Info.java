1da85d52fdf221300802ffd0f4f413a15c00583d
LovesAsuna <625924077@qq.com>
2020-02-04 09:43:28 +0800

    [+] 略微修改info命令

--- src/main/java/org/sct/lock/command/sub/Info.java
+++ src/main/java/org/sct/lock/command/sub/Info.java
@@ -8,7 +8,7 @@ import org.sct.lock.util.SubCommand;
 public class Info implements SubCommand {
     @Override
     public boolean execute(CommandSender sender, String[] args) {
-        sender.sendMessage("§7┌§e§lLock");
+        sender.sendMessage("§7┌§ePlugin§7:§b Lock");
         sender.sendMessage("§7├§eAuthor§7:§b 冰星");
         sender.sendMessage("§7├§eVersion§7:§b " + Lock.getInstance().getDescription().getVersion());
         sender.sendMessage("§7└§eLink§7:§b https://www.mcbbs.net/thread-932739-1-1.html");

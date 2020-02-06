0ab37f0c077a6ab9b313d9e7f3814ad50601c7b6
LovesAsuna <625924077@qq.com>
2020-02-07 00:34:07 +0800

    [+]

---
+++ src/test/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(1);
+        System.out.println(buffer.toString());
+    }
+}

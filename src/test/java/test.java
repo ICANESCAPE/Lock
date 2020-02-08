505c0dd38f0f187f75099ac24d4a489db768808b
LovesAsuna <625924077@qq.com>
2020-02-08 13:29:03 +0800

    [+]

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,19 @@
+import org.sct.lock.util.BasicUtil;
+
 public class test {
     public static void main(String[] args) {
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(1);
-        System.out.println(buffer.toString());
+        String line = ">+";
+        int detect = 0;
+        if (line.contains("<")) {
+            System.out.println(1);
+            detect++;
+        } else if (line.contains(">")) {
+            System.out.println(2);
+            detect++;
+        } else if (line.contains("=")) {
+            System.out.println(3);
+            detect++;
+        }
+        System.out.println(detect);
     }
 }
